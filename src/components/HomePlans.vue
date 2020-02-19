83b590e6c8d22c12a40aa2d3116449d2f7925258
akdev5 <akio.mori555@gmail.com>
2020-02-19 10:58:45 +0200

    update homepage

---
+++ src/components/HomePlans.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Advantages</v-flex>
+        </v-layout>
+        <v-layout row wrap mb-5>
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/hRVrvH9-dG0" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Lorem ipsum</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Lorem ipsum</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/8gn_WhKv8Ns" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Lorem ipsum</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Lorem ipsum</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/lwaw_DL09S4" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Lorem ipsum</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Lorem ipsum</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomePlans'
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
